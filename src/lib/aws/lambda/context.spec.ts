758c8f31d2a65f16b62af3f1f26d3e68f3e4cd1c
Paul Visscher <p.e.visscher@skyleague.io>
2022-11-29 00:15:59 +0100

    feat: add lambda schemas

---
+++ src/lib/aws/lambda/context.spec.ts
@@ -0,0 +1,8 @@
+import type { Context } from './context.type'
+
+import type { Context as LambdaContext } from 'aws-lambda'
+
+test('type is compatible', () => {
+    const _test: Context = {} as unknown as LambdaContext
+    // const _test2: LambdaContext = {} as unknown as Context
+})
