3aa8b7708872256cb26d0b1569fd1bd0798218df
pevisscher <2388946+pevisscher@users.noreply.github.com>
2022-11-30 10:50:21 +0100

    feat: add sqs schemas (#17)
    * chore: upgrade dependencies
    
    * feat: add apigateway schemas
    
    * feat: add eventbridge schemas
    
    * feat: add firehose schemas
    
    * feat: add lambda schemas
    
    * feat: add kinesis schemas
    
    * feat: add s3 schemas
    
    * feat: add s3 batch schemas
    
    * feat: add secret rotation schemas
    
    * feat: add sns schemas
    
    * feat: add sqs schemas

---
+++ src/lib/aws/sqs/sqs.spec.ts
@@ -0,0 +1,8 @@
+import type { SQSRecord } from './sqs.type'
+
+import type { SQSRecord as LambdaSqsRecord } from 'aws-lambda'
+
+test('type is compatible', () => {
+    const _test: SQSRecord = {} as unknown as LambdaSqsRecord
+    // const _test2: LambdaSqsRecord = {} as unknown as SQSRecord
+})
