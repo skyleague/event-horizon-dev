d8aea06bb6df087378c1db86565739eec479358f
pevisscher <2388946+pevisscher@users.noreply.github.com>
2022-11-30 10:49:21 +0100

    feat: add sns schemas (#16)
    * chore: upgrade dependencies
    
    * feat: add apigateway schemas
    
    * feat: add eventbridge schemas
    
    * feat: add firehose schemas
    
    * feat: add lambda schemas
    
    * feat: add kinesis schemas
    
    * feat: add s3 schemas
    
    * feat: add s3 batch schemas
    
    * feat: add secret rotation schemas
    
    * feat: add sns schemas

---
+++ src/lib/aws/sns/sns.spec.ts
@@ -0,0 +1,8 @@
+import type { SNSEvent } from './sns.type'
+
+import type { SNSEvent as LambdaSnsEvent } from 'aws-lambda'
+
+test('type is compatible', () => {
+    const _test: SNSEvent = {} as unknown as LambdaSnsEvent
+    // const _test2: LambdaSnsEvent = {} as unknown as SNSEvent
+})
