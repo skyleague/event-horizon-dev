efed4e79e22200bba9e4499a17d57ae276690ca3
pevisscher <2388946+pevisscher@users.noreply.github.com>
2022-12-18 20:22:25 +0100

    refactor: upgrade dependencies (#28)

--- src/lib/schema/headers/headers.ts
+++ src/lib/schema/headers/headers.ts
@@ -1,8 +1,8 @@
-import type { CstNode, ObjectPropertiesArg, SchemaOptions, LazyObjectOptions } from '@skyleague/therefore'
-import { $validator, isCstNode, $object } from '@skyleague/therefore'
+import type { ThereforeNode, ObjectPropertiesArg, SchemaOptions, LazyObjectOptions } from '@skyleague/therefore'
+import { $validator, isThereforeNode, $object } from '@skyleague/therefore'
 
-export function $headers(properties: CstNode | ObjectPropertiesArg, options?: SchemaOptions<LazyObjectOptions>) {
-    const validator = $validator(isCstNode(properties) ? properties : $object(properties, options))
+export function $headers(properties: ObjectPropertiesArg | ThereforeNode, options?: SchemaOptions<LazyObjectOptions>) {
+    const validator = $validator(isThereforeNode(properties) ? properties : $object(properties, options))
     validator.description.ajvOptions = { ...validator.description.ajvOptions, coerceTypes: true }
     return validator
 }
--- src/lib/schema/query/query.ts
+++ src/lib/schema/query/query.ts
@@ -1,8 +1,8 @@
-import type { CstNode, LazyObjectOptions, ObjectPropertiesArg, SchemaOptions } from '@skyleague/therefore'
-import { $object, $validator, isCstNode } from '@skyleague/therefore'
+import type { ThereforeNode, LazyObjectOptions, ObjectPropertiesArg, SchemaOptions } from '@skyleague/therefore'
+import { $object, $validator, isThereforeNode } from '@skyleague/therefore'
 
-export function $query(properties: CstNode | ObjectPropertiesArg, options?: SchemaOptions<LazyObjectOptions>) {
-    const validator = $validator(isCstNode(properties) ? properties : $object(properties, options))
+export function $query(properties: ObjectPropertiesArg | ThereforeNode, options?: SchemaOptions<LazyObjectOptions>) {
+    const validator = $validator(isThereforeNode(properties) ? properties : $object(properties, options))
     validator.description.ajvOptions = { ...validator.description.ajvOptions, coerceTypes: true }
     return validator
 }
