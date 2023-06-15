5526546bea667cb2f03f39a6e12bdee0b1135f16
Paul Visscher <p.e.visscher@skyleague.io>
2023-06-15 11:05:24 +0200

    fix(sns): update sns record type to latest version

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
         setupFiles: ['./test/__test__/setup.ts'],
+        include: ['./src/**/*.{test,spec}.{ts,js}', './test/**/*.{test,spec}.{ts,js}'],
         coverage: {
             reportsDirectory: './.coverage',
         },
