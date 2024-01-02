629c86bfb112ed4478c152178f947ade6d48b0b3
Mick van Duijn <m.vanduijn@zefiros.io>
2024-01-02 14:34:12 +0100

    fix(sqs): implement message grouping settings

--- src/lib/event-horizon/sqs/sqs.ts
+++ src/lib/event-horizon/sqs/sqs.ts
@@ -1,16 +1,45 @@
 import { SQSRecord } from '../../aws/sqs/sqs.type.js'
 
 import type { Dependent } from '@skyleague/axioms'
-import { object, unknown } from '@skyleague/axioms'
-import type { SQSEvent, SQSHandler } from '@skyleague/event-horizon'
+import { alphaNumeric, array, constant, integer, object, tuple, unknown } from '@skyleague/axioms'
+import type { SQSEvent, SQSHandler, SQSPayload } from '@skyleague/event-horizon'
 import { arbitrary } from '@skyleague/therefore'
 
-export function sqsEvent<Configuration, Service, Profile, Payload>(
-    definition: SQSHandler<Configuration, Service, Profile, Payload>
-): Dependent<SQSEvent<Payload>> {
+export function sqsEvent<Configuration, Service, Profile, Payload, MessageGrouping extends { by?: 'message-group-id' }>(
+    definition: SQSHandler<Configuration, Service, Profile, Payload, MessageGrouping>
+): Dependent<SQSPayload<Payload, MessageGrouping>> {
     const { sqs } = definition
-    return object({
-        payload: sqs.schema.payload !== undefined ? arbitrary(sqs.schema.payload) : unknown(),
-        raw: arbitrary(SQSRecord),
-    }) as unknown as Dependent<SQSEvent<Payload>>
+    if (sqs.messageGrouping !== undefined) {
+        return object({
+            messageGroupId: alphaNumeric({ minLength: 1 }),
+            payload: sqs.schema.payload !== undefined ? arbitrary(sqs.schema.payload) : unknown(),
+            raw: arbitrary(SQSRecord),
+            item: integer({ min: 0, max: 10 }),
+        } satisfies { [k in keyof SQSEvent]: unknown }) as Dependent<SQSPayload<Payload, MessageGrouping>>
+    } else {
+        return tuple(
+            array(
+                object({
+                    // this will be overwritten by the map below
+                    messageGroupId: constant(''),
+                    item: constant(0),
+
+                    payload: sqs.schema.payload !== undefined ? arbitrary(sqs.schema.payload) : unknown(),
+                    raw: arbitrary(SQSRecord),
+                } satisfies { [k in keyof SQSEvent]: unknown }),
+                { minLength: 1, maxLength: 10 }
+            ),
+            alphaNumeric({ minLength: 1 })
+        ).map(([records, messageGroupId]) => ({
+            records: records.map(
+                (r, item) =>
+                    ({
+                        ...r,
+                        messageGroupId,
+                        item,
+                    }) satisfies { [k in keyof SQSEvent]: unknown }
+            ),
+            messageGroupId,
+        })) as Dependent<SQSPayload<Payload, MessageGrouping>>
+    }
 }
